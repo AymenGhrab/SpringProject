7362c16d015f18dd4e8804edb3495695df86bfac
AymenGhrab <aymenghrab3@gmail.com>
2023-11-19 22:18:09 +0100

    seance10

---
+++ src/main/java/tn/esprit/com/foyer/FoyerApplication.java
@@ -0,0 +1,14 @@
+package tn.esprit.com.foyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class FoyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoyerApplication.class, args);
+    }
+
+}
